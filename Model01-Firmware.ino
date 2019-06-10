d59c146aa4099674af5556f627526b26d6260a9f
Dan Cardamore <dan@hld.ca>
2019-06-10 15:19:34 -0400

    prog becomes escape.  swap shift keys around

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -170,19 +170,35 @@ enum { PRIMARY, NUMPAD, FUNCTION }; // layers
 KEYMAPS(
 
 #if defined (PRIMARY_KEYMAP_QWERTY)
+		/* original settings */
+  /* [PRIMARY] = KEYMAP_STACKED */
+  /* (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, */
+  /*  Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab, */
+  /*  Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G, */
+  /*  Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape, */
+  /*  Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift, */
+  /*  ShiftToLayer(FUNCTION), */
+
+  /*  M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD), */
+  /*  Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals, */
+  /*                 Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote, */
+  /*  Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus, */
+  /*  Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl, */
+  /*  ShiftToLayer(FUNCTION)), */
+
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+  (Key_Escape,   Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+   Key_LeftControl, Key_Backspace, Key_LeftShift, Key_LeftGui,
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   Key_LeftAlt, Key_RightShift, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
 #elif defined (PRIMARY_KEYMAP_DVORAK)
@@ -196,7 +212,7 @@ KEYMAPS(
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),   Key_6, Key_7, Key_8, Key_9, Key_0, LockLayer(NUMPAD),
-   Key_Enter,      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
+   Key_Enter,      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slas,
                    Key_D, Key_H, Key_T, Key_N, Key_S, Key_Minus,
    Key_RightAlt,   Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
